55ae4bf20ec234c5b8257c60442a2e2cf0a3775e
Zelen <alexandre_zelen@yahoo.fr>
2021-10-09 17:36:54 +0200

    fonction pour chaque élément

--- js/product.js
+++ js/product.js
@@ -4,10 +4,12 @@ const formatter = new Intl.NumberFormat('fr-FR', {
     currency: 'EUR',
 });
 
-// retrieve URL
+// -- retrieve URL -- //
+
 const url = new URLSearchParams(window.location.search);
 
-// stock id
+// -- store id -- //
+
 const id = url.get("_id");
 console.log(id);
 console.log(server + "/" + id);
@@ -18,105 +20,85 @@ fetch(server + "/" + id)
     .catch((err) => console.log(err));
 
 function showContent(camera) {
-    let cameraElt = document.createElement("div");
-    let titleElt = document.createElement("h2");
-    let descriptionElt = document.createElement("span");
-    let priceElt = document.createElement("span");
-    let imageElt = document.createElement("img");
+    let cameraElt = createCameraElement(camera.element);
+    let titleElt = createTitleElement(camera.title);
+    let descriptionElt = createDescriptionElement(camera.description);
+    let priceElt = createPriceElement(camera.price);
+    let imageElt = createImageElement(camera.image);
     let lensesElt = createSelectForLenses(camera.lenses);
+    let buttonElt = createButtonElement(camera.button);
     titleElt.innerHTML = camera.name;
     descriptionElt.innerHTML = camera.description;
     priceElt.innerHTML = formatter.format((camera.price) / 100);
     imageElt.src = camera.imageUrl;
     imageElt.classList.add("cameraPicture");
-    lensesElt.classList.add("cameraLenses");
+    buttonElt.innerHTML = camera.button;
     cameraElt.appendChild(titleElt);
     cameraElt.appendChild(descriptionElt);
     cameraElt.appendChild(priceElt);
     cameraElt.appendChild(imageElt);
     cameraElt.appendChild(lensesElt);
+    cameraElt.appendChild(buttonElt);
     document.getElementsByTagName("main")[0].appendChild(cameraElt);
 }
 
-function createSelectForLenses(listOfLenses) {
-    let generatedHtml = document.createElement("select");
-    listOfLenses.forEach((lens) => {
-        let option = document.createElement("option");
-        option.text = lens;
-        generatedHtml.add(option);
-    });
+// -- functions for elements of camera -- //
+
+function createCameraElement() {
+    let generatedHtml;
+    generatedHtml = document.createElement("div");
     return generatedHtml;
 }
 
-// select addToCart button
-const btn_addToShoppingCart = document.querySelector("#btn_addToShoppingCart");
-console.log(btn_addToShoppingCart)
+// -- function title -- //
 
-// send to localStorage
-const addToLocalStorage = shoppingCart => {
-    localStorage.setItem('shoppingCart', JSON.stringify(shoppingCart));
-};
-
-// options choice
-for (let lenses of camera.lenses) {
-    document.getElementsByClassName('cameraLenses').innerHTML +=
-        `option value="1">${lenses}</option`
+function createTitleElement() {
+    let generatedHtml;
+    generatedHtml = document.createElement("h2");
+    return generatedHtml;
 }
 
-// add to shoppingCart fonction
+// -- function description -- //
 
-const add
-
-// addEventListener
-btn_addToCart.addEventListener("click", (event) => {
-    event.preventDefault();
-
-    let items = ["name", "description", "price", "cameraPicture", "lenses"];
-    localStorage.setItem("myItems", JSON.stringify(items)); //store items
-    let storedItems = JSON.parse(localStorage.getItem("myItems")); //get them back
-    console.log(storedItems)
-});
-
-/* --------------------------------------------------------------
-
-function addEntry() {
-    // Parse any JSON previously stored in allEntries
-    var existingEntries = JSON.parse(localStorage.getItem("allEntries"));
-    if (existingEntries == null) existingEntries = [];
-    var entryTitle = document.getElementById("entryTitle").value;
-    var entryText = document.getElementById("entryText").value;
-    var entry = {
-        "title": entryTitle,
-        "text": entryText
-    };
-    localStorage.setItem("entry", JSON.stringify(entry));
-    // Save allEntries back to local storage
-    existingEntries.push(entry);
-    localStorage.setItem("allEntries", JSON.stringify(existingEntries));
+function createDescriptionElement() {
+    let generatedHtml;
+    generatedHtml = document.createElement("span");
+    return generatedHtml;
 }
 
- ------------------------------------------ */
+// -- function price -- //
 
+function createPriceElement() {
+    let generatedHtml;
+    generatedHtml = document.createElement("span");
+    return generatedHtml;
+}
 
-/* -------------------------------------------
+// -- function image -- //
 
-var testObject = {'one': 1, 'two': 2, 'three': 3};
+function createImageElement() {
+    let generatedHtml;
+    generatedHtml = document.createElement("img");
+    return generatedHtml;
 
-// Put the object into storage
-localStorage.setItem('testObject', JSON.stringify(testObject));
+}
 
-// Retrieve the object from storage
-var retrievedObject = localStorage.getItem('testObject');
+// -- function lenses -- //
 
-console.log('retrievedObject: ', JSON.parse(retrievedObject));
+function createSelectForLenses(listOfLenses) {
+    let generatedHtml = document.createElement("select");
+    for (let i = 0; i < listOfLenses.length; i++) {
+        let option = document.createElement("option");
+        option.text = listOfLenses[i];
+        generatedHtml.add(option);
+    }
+    return generatedHtml;
+}
 
------------------------------------------------- */
+// -- function button -- //
 
-
-
-
-
-
-
-
-
+function createButtonElement() {
+    let generatedHtml;
+    generatedHtml = document.createElement("button");
+    return generatedHtml;
+}
