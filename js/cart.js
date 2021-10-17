ec9854e61b7425e991afa39c247e4655db30f2a7
Zelen <alexandre_zelen@yahoo.fr>
2021-10-17 23:38:05 +0200

    css

--- js/cart.js
+++ js/cart.js
@@ -1,179 +1,196 @@
+const order = document.getElementById("order");
+const regexName = /^(([a-zA-ZÀ-ÿ]+[\s\-]{1}[a-zA-ZÀ-ÿ]+)|([a-zA-ZÀ-ÿ]+))$/;
+const regexCity = /^(([a-zA-ZÀ-ÿ]+[\s\-]{1}[a-zA-ZÀ-ÿ]+)|([a-zA-ZÀ-ÿ]+)){1,10}$/;
+const regexMail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]{2,}\.[a-z]{2,4}$/;
+const regexAddress = /^(([a-zA-ZÀ-ÿ0-9]+[\s\-]{1}[a-zA-ZÀ-ÿ0-9]+)){1,10}$/;
+const checkBox = document.getElementById("invalidCheck2");
+
+if (
+    (regexMail.test(contact.email) == true) &
+    (regexName.test(contact.firstName) == true) &
+    (regexName.test(contact.lastName) == true) &
+    (regexCity.test(contact.city) == true) &
+    (regexAddress.test(contact.address) == true) &
+    (checkBox.checked == true)
+) {
+    event.preventDefault();
+
 // -- Const -- //
 
-const formatter = new Intl.NumberFormat('fr-FR', {
-    style: 'currency',
-    currency: 'EUR',
-});
+    const formatter = new Intl.NumberFormat('fr-FR', {
+        style: 'currency',
+        currency: 'EUR',
+    });
 
-let paiement = document.getElementById("paiement");
+    let paiement = document.getElementById("paiement");
 
 // -- event listener -- //
-/*
-paiement.addEventListener("click", (event) => {
-        console.log("paiement")
-        event.preventDefault();
+    /*
+    paiement.addEventListener("click", (event) => {
+            console.log("paiement")
+            event.preventDefault();
 
-        // -- Prepare data to send to Post -- //
+            // -- Prepare data to send to Post -- //
 
-        let contact = {
-            firstName: document.getElementById("firstName").value,
-            lastName: document.getElementById("lastName").value,
-            address: document.getElementById("address").value,
-            city: document.getElementById("city").value,
-            email: document.getElementById("eMail").value,
-        };
-        console.log("hello", contact)
+            let contact = {
+                firstName: document.getElementById("firstName").value,
+                lastName: document.getElementById("lastName").value,
+                address: document.getElementById("address").value,
+                city: document.getElementById("city").value,
+                email: document.getElementById("eMail").value,
+            };
+            console.log("hello", contact)
 
-        let cart = JSON.parse(localStorage.getItem("cart"));
-        if (!cart) {
-            return
-        }
-        console.log("coucou", cart)
-        let products = cart.map(product => product._id)
-        console.log(JSON.stringify({contact, products}))
-        // -- Post -- //
+            let cart = JSON.parse(localStorage.getItem("cart"));
+            if (!cart) {
+                return
+            }
+            console.log("coucou", cart)
+            let products = cart.map(product => product._id)
+            console.log(JSON.stringify({contact, products}))
+            // -- Post -- //
 
-        fetch("http://localhost:3000/api/cameras/order", {
-            method: "POST",
-            headers: {
-                "accept": "application.json",
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({contact, products})
-        })
-            .then((response) => response.json())
-            .then((data) => {
-                console.log(data.orderId)
-                document.location.href = "./confirmation.html?orderId=" + data.orderId;
+            fetch("http://localhost:3000/api/cameras/order", {
+                method: "POST",
+                headers: {
+                    "accept": "application.json",
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({contact, products})
             })
-            .catch((erreur) => console.log("erreur:" + erreur));
+                .then((response) => response.json())
+                .then((data) => {
+                    console.log(data.orderId)
+                    document.location.href = "./confirmation.html?orderId=" + data.orderId;
+                })
+                .catch((erreur) => console.log("erreur:" + erreur));
 
-    }
-) */
+        }
+    ) */
 
 // -- function clearBasket -- //
 
-function initClearBasketListener() {
-    const buttonClearBasket = document.getElementById("clearButton");
-    buttonClearBasket.addEventListener("click", () => {
-        localStorage.setItem("cart", null)
-        location.reload();
-    });
-}
+    function initClearBasketListener() {
+        const buttonClearBasket = document.getElementById("clearButton");
+        buttonClearBasket.addEventListener("click", () => {
+            localStorage.setItem("cart", null)
+            location.reload();
+        });
+    }
 
 // -- Empty basket message -- //
 
-function createCartElement(cart) {
-    let cartMessage = document.createElement("h2");
-    if (cart) {
-        cartMessage.textContent = "Contenu de votre panier";
-    } else {
-        cartMessage.textContent = "Votre panier est vide";
+    function createCartElement(cart) {
+        let cartMessage = document.createElement("h2");
+        if (cart) {
+            cartMessage.textContent = "Contenu de votre panier";
+        } else {
+            cartMessage.textContent = "Votre panier est vide";
+        }
+        return cartMessage
     }
-    return cartMessage
-}
 
-function showCartShopping() {
-    let cart = JSON.parse(localStorage.getItem("cart"))
-    let cartMessageElement = createCartElement(cart)
-    let cartSection = document.getElementById("cartSection")
-    cartSection.appendChild(cartMessageElement)
-    if (cart) {
-        cart.forEach((camera) => {
-            let displayCartShoppingElement = document.createElement("div")
-            let titleElt = document.createElement("h3");
-            let imageElt = document.createElement("img");
-            let lensesElt = document.createElement("span");
-            let priceElt = document.createElement("span");
-            titleElt.innerHTML = camera.name;
-            imageElt.src = camera.imageUrl;
-            imageElt.classList.add("cameraPicture");
-            lensesElt.innerHTML = camera.selectedLens;
-            priceElt.innerHTML = formatter.format((camera.price) / 100);
-            priceElt.classList.add("priceElt");
-            cartSection.appendChild(displayCartShoppingElement);
-            displayCartShoppingElement.appendChild(titleElt);
-            displayCartShoppingElement.appendChild(imageElt);
-            displayCartShoppingElement.appendChild(lensesElt);
-            displayCartShoppingElement.appendChild(priceElt);
-        })
+    function showCartShopping() {
+        let cart = JSON.parse(localStorage.getItem("cart"))
+        let cartMessageElement = createCartElement(cart)
+        let cartSection = document.getElementById("cartSection")
+        cartSection.appendChild(cartMessageElement)
+        if (cart) {
+            cart.forEach((camera) => {
+                let displayCartShoppingElement = document.createElement("div")
+                let titleElt = document.createElement("h3");
+                let imageElt = document.createElement("img");
+                let lensesElt = document.createElement("span");
+                let priceElt = document.createElement("span");
+                titleElt.innerHTML = camera.name;
+                imageElt.src = camera.imageUrl;
+                imageElt.classList.add("cameraPicture");
+                lensesElt.innerHTML = camera.selectedLens;
+                priceElt.innerHTML = formatter.format((camera.price) / 100);
+                priceElt.classList.add("priceElt");
+                cartSection.appendChild(displayCartShoppingElement);
+                displayCartShoppingElement.appendChild(titleElt);
+                displayCartShoppingElement.appendChild(imageElt);
+                displayCartShoppingElement.appendChild(lensesElt);
+                displayCartShoppingElement.appendChild(priceElt);
+            })
 
-        paiement.addEventListener("click", (event) => {
-                console.log("paiement")
-                event.preventDefault();
+            paiement.addEventListener("click", (event) => {
+                    console.log("paiement")
+                    event.preventDefault();
 
-                // -- Prepare data to send to Post -- //
+                    // -- Prepare data to send to Post -- //
 
-                let contact = {
-                    firstName: document.getElementById("firstName").value,
-                    lastName: document.getElementById("lastName").value,
-                    address: document.getElementById("address").value,
-                    city: document.getElementById("city").value,
-                    email: document.getElementById("eMail").value,
-                };
-                console.log("hello", contact)
+                    let contact = {
+                        firstName: document.getElementById("firstName").value,
+                        lastName: document.getElementById("lastName").value,
+                        address: document.getElementById("address").value,
+                        city: document.getElementById("city").value,
+                        email: document.getElementById("eMail").value,
+                    };
+                    console.log("hello", contact)
 
-                let cart = JSON.parse(localStorage.getItem("cart"));
-                if (!cart) {
-                    return
-                }
-                console.log("coucou", cart)
-                let products = cart.map(product => product._id)
-                console.log(JSON.stringify({contact, products}))
-                // -- Post -- //
+                    let cart = JSON.parse(localStorage.getItem("cart"));
+                    if (!cart) {
+                        return
+                    }
+                    console.log("coucou", cart)
+                    let products = cart.map(product => product._id)
+                    console.log(JSON.stringify({contact, products}))
+                    // -- Post -- //
 
-                fetch("http://localhost:3000/api/cameras/order", {
-                    method: "POST",
-                    headers: {
-                        "accept": "application.json",
-                        "Content-Type": "application/json"
-                    },
-                    body: JSON.stringify({contact, products})
-                })
-                    .then((response) => response.json())
-                    .then((data) => {
-                        console.log(data.orderId)
-                        document.location.href = "./confirmation.html?orderId=" + data.orderId + "&totalCount=" + totalPrice;
+                    fetch("http://localhost:3000/api/cameras/order", {
+                        method: "POST",
+                        headers: {
+                            "accept": "application.json",
+                            "Content-Type": "application/json"
+                        },
+                        body: JSON.stringify({contact, products})
                     })
-                    .catch((erreur) => console.log("erreur:" + erreur));
+                        .then((response) => response.json())
+                        .then((data) => {
+                            console.log(data.orderId)
+                            document.location.href = "./confirmation.html?orderId=" + data.orderId + "&totalCount=" + totalPrice;
+                        })
+                        .catch((erreur) => console.log("erreur:" + erreur));
 
-            }
-        )
+                }
+            )
 
-        // -- set variable in order to insert the prices existing in the cart shopping -- //
+            // -- set variable in order to insert the prices existing in the cart shopping -- //
 
-        let finalPrice = [];
+            let finalPrice = [];
 
 // -- search prices in the cart shopping -- //
 
-        for (let i = 0; i < cart.length; i++) {
-            let finalPriceInTheCartShopping = cart[i].price / 100;
+            for (let i = 0; i < cart.length; i++) {
+                let finalPriceInTheCartShopping = cart[i].price / 100;
 
-            // -- put prices of cart shopping in the variable -- //
+                // -- put prices of cart shopping in the variable -- //
 
-            finalPrice.push(finalPriceInTheCartShopping)
+                finalPrice.push(finalPriceInTheCartShopping)
 
-            console.log(finalPrice);
+                console.log(finalPrice);
+            }
+
+            // -- add prices existing in the variable with method reduce -- //
+
+            const reducer = (accumulator, currentValue) => accumulator + currentValue;
+            const totalPrice = finalPrice.reduce(reducer);
+            console.log(totalPrice);
+
+            // -- display totalPrice to HTML -- //
+
+            const showFinalPriceToHtml = `<div id= "showFinalPrice">Le prix total est de : ${totalPrice} € </div>`
+
+            const insertFinalPrice = document.querySelector("#finalPrice");
+            console.log(insertFinalPrice);
+
+            // -- insert HTML in the cart shopping -- //
+
+            insertFinalPrice.insertAdjacentHTML("beforeend", showFinalPriceToHtml);
         }
-
-        // -- add prices existing in the variable with method reduce -- //
-
-        const reducer = (accumulator, currentValue) => accumulator + currentValue;
-        const totalPrice = finalPrice.reduce(reducer);
-        console.log(totalPrice);
-
-        // -- display totalPrice to HTML -- //
-
-        const showFinalPriceToHtml = `<div id= "showFinalPrice">Le prix total est de : ${totalPrice} € </div>`
-
-        const insertFinalPrice = document.querySelector("#finalPrice");
-        console.log(insertFinalPrice);
-
-        // -- insert HTML in the cart shopping -- //
-
-        insertFinalPrice.insertAdjacentHTML("beforeend", showFinalPriceToHtml);
+        initClearBasketListener()
     }
-    initClearBasketListener()
-}
 
-showCartShopping()
+    showCartShopping()
--- js/confirmation.js
+++ js/confirmation.js
@@ -2,5 +2,5 @@ const url = new URLSearchParams(window.location.search);
 const id = url.get("orderId");
 const totalCount = url.get("totalCount");
 console.log("hello", totalCount)
-document.getElementById("message").innerHTML = "Nous vous remercions pour votre achat. Le numéro de référence est le : " + id + ". Le prix de votre panier est de : " + totalCount + " €";
+document.getElementById("message").innerHTML = "Nous vous remercions pour votre achat! </br> Le numéro de référence est le : " + id + ".</br>Le prix de votre panier est de : " + totalCount + " €.";
 
