159ef1159f436e6b1410b6175b7c59edd8cfc4a9
Zelen <alexandre_zelen@yahoo.fr>
2021-10-18 10:59:33 +0200

    css

--- js/confirmation.js
+++ js/confirmation.js
@@ -2,5 +2,4 @@ const url = new URLSearchParams(window.location.search);
 const id = url.get("orderId");
 const totalCount = url.get("totalCount");
 console.log("hello", totalCount)
-document.getElementById("message").innerHTML = "Nous vous remercions pour votre achat! </br> Le numéro de référence est le : " + id + ".</br>Le prix de votre panier est de : " + totalCount + " €.";
-
+document.getElementById("message").innerHTML = "Nous vous remercions pour votre achat! &#128522</br> Le numéro de référence est le : " + id + ".</br>Le prix total de votre commande est de : " + totalCount + " €.";
--- js/utils.js
+++ js/utils.js
@@ -1,3 +1,5 @@
+// -- Intl.NumberFormat object enables language-sensitive number formatting -- //
+
 const formatter = new Intl.NumberFormat('fr-FR', {
     style: 'currency',
     currency: 'EUR',
